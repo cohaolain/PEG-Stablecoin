33bc1459adb9dccfd8026e905daf9419ff5d5516
Ciarán Ó hAoláin <ciaran@cohaolain.ie>
2020-04-13 13:23:54 +0100

    Update documentation page

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,28 +1,100 @@
 import React, { Component } from "react";
 
-import { Grid, Header, Icon, Segment, Placeholder } from "semantic-ui-react";
+import { Grid, Header, Icon, Segment, Table } from "semantic-ui-react";
 
 import Content from "./Content.js";
 
+import docs from "../assets/docs.json";
+
 export default class HowItWorks extends Component {
+	getMethodElements = () => {
+		let elements = [];
+		for (let key in docs.methods) {
+			elements.push(
+				<Segment key={key}>
+					<h4>{key}</h4>
+					{docs.methods[key].params ? (
+						<Table celled>
+							<Table.Header>
+								<Table.Row>
+									<Table.HeaderCell>
+										Parameters
+									</Table.HeaderCell>
+									<Table.HeaderCell>
+										Explanation
+									</Table.HeaderCell>
+								</Table.Row>
+							</Table.Header>
+
+							<Table.Body>
+								{Object.keys(docs.methods[key].params).map(
+									(k) => (
+										<Table.Row key={k}>
+											<Table.Cell>{k}</Table.Cell>
+											<Table.Cell>
+												{docs.methods[key].params[k]}
+											</Table.Cell>
+										</Table.Row>
+									)
+								)}
+							</Table.Body>
+						</Table>
+					) : (
+						""
+					)}
+					<p>{docs.methods[key].details}</p>
+					{docs.methods[key].returns ? (
+						<Table celled>
+							<Table.Header>
+								<Table.Row>
+									<Table.HeaderCell>
+										Return value
+									</Table.HeaderCell>
+									<Table.HeaderCell>
+										Explanation
+									</Table.HeaderCell>
+								</Table.Row>
+							</Table.Header>
+
+							<Table.Body>
+								{Object.keys(docs.methods[key].returns).map(
+									(k) => (
+										<Table.Row key={k}>
+											<Table.Cell>{k}</Table.Cell>
+											<Table.Cell>
+												{docs.methods[key].returns[k]}
+											</Table.Cell>
+										</Table.Row>
+									)
+								)}
+							</Table.Body>
+						</Table>
+					) : (
+						""
+					)}
+				</Segment>
+			);
+		}
+		return elements;
+	};
+
 	render() {
+		window.docs = docs;
 		return (
 			<Content>
-				<Grid centered verticalAlign="middle">
+				<Grid centered verticalAlign="middle" style={{ maxWidth: 750 }}>
 					<Grid.Column verticalAlign="middle">
 						<Header as="h2" icon>
 							<Icon name="book" />
 							Documentation
 						</Header>
-						<Segment>
-							<p>These are the docs</p>
-							<Placeholder>
-								<Placeholder.Line />
-								<Placeholder.Line />
-								<Placeholder.Line />
-								<Placeholder.Line />
-								<Placeholder.Line />
-							</Placeholder>
+						<Segment
+							style={{
+								textAlign: "left",
+							}}
+						>
+							<h1>{docs.title}</h1>
+							{this.getMethodElements()}
 						</Segment>
 					</Grid.Column>
 				</Grid>
