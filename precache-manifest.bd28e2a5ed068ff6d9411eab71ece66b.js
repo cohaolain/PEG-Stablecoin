4a325328a418ac8a33e76b52f2ce4ce3065c8e82
Ciarán Ó hAoláin <ciaran@cohaolain.ie>
2020-05-09 00:40:15 +0100

    Add release

---
+++ precache-manifest.bd28e2a5ed068ff6d9411eab71ece66b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea4aa00f461e1291371b0797921bfa5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e50a92a8ba8e2fcd81d2",
+    "url": "./static/css/main.986d4a14.chunk.css"
+  },
+  {
+    "revision": "066037f1cce4a03945ce",
+    "url": "./static/js/2.4e4e7041.chunk.js"
+  },
+  {
+    "revision": "e50a92a8ba8e2fcd81d2",
+    "url": "./static/js/main.9f06c5b9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7077a0c5278d87cde6dabb55e7c31abe",
+    "url": "./static/media/ciaran-avatar.7077a0c5.svg"
+  },
+  {
+    "revision": "0108c89ad195c15437d6d5c1728fd691",
+    "url": "./static/media/phil-avatar.0108c89a.svg"
+  }
+]);
